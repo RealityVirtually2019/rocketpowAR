80bc58644b513fffab312962731172c7dfe009ee
Rogue <jnf256@nyu.edu>
2019-01-19 19:32:51 -0500

    Baseline game logic

---
+++ UnityClient/Assets/Scripts/ConveyerBelt.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour {
+
+    public GameObject ConveyerBeltCenter;
+    public Vector3 conveyerBeltBelowElevator;
+    public Vector3 conveyerBeltRaised;
+
+    public AudioSource conveyerBeltRaiseSFX;
+    public AudioSource conveyerBeltLowerSFX;
+    public AudioSource conveyerBeltRotateSFX;
+
+	// Use this for initialization
+	void Start () {
+        ConveyerBeltCenter.transform.position = conveyerBeltBelowElevator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //for testing purposes
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RotateConveyerBeltRight();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            RotateConveyerBeltLeft();
+        }*/
+	}
+
+    public void RaiseConveyerBelt()
+    {
+        if (GameFlowManager.Instance.CurrentGameState == GlobalGameState.SELECTEXERCISE)
+        {
+            conveyerBeltRaiseSFX.Play();
+            ConveyerBeltCenter.transform.position = Vector3.Lerp(conveyerBeltBelowElevator, conveyerBeltRaised, 2000f);
+        }
+    }
+
+    public void LowerConveyerBelt()
+    {
+        if (GameFlowManager.Instance.CurrentGameState == GlobalGameState.SELECTEXERCISE)
+        {
+            conveyerBeltLowerSFX.Play();
+            ConveyerBeltCenter.transform.position = Vector3.Lerp(conveyerBeltRaised, conveyerBeltBelowElevator, 2000f);
+        }
+    }
+
+    public void RotateConveyerBeltRight()
+    {
+        if (GameFlowManager.Instance.CurrentGameState == GlobalGameState.SELECTEXERCISE && !conveyerBeltRotateSFX.isPlaying)
+        {
+            ConveyerBeltCenter.transform.Rotate(gameObject.transform.position, 45f);
+        }
+    }
+
+    public void RotateConveyerBeltLeft()
+    {
+        if (GameFlowManager.Instance.CurrentGameState == GlobalGameState.SELECTEXERCISE && !conveyerBeltRotateSFX.isPlaying)
+        {
+            ConveyerBeltCenter.transform.Rotate(gameObject.transform.position, -45f);
+        }
+    }
+}
---
+++ UnityClient/Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowManager : Singleton<GameFlowManager> {
+
+    public GameObject rocketObject;
+    public int rocketsToLaunch;
+    private int rocketsLaunched;
+    public GlobalGameState CurrentGameState;
+
+    // Use this for initialization
+    void Start () {
+        rocketsLaunched = 0;
+        CurrentGameState = GlobalGameState.NOTSTARTED;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void IncrementRocket()
+    {
+        if(CurrentGameState == GlobalGameState.LAUNCHROCKETS)
+        {
+            if (rocketsLaunched < rocketsToLaunch)
+            {
+                rocketsLaunched++;
+            }
+        }
+    }
+
+}
+
+public enum GlobalGameState
+{
+    NOTSTARTED, PLACEELEVATOR, SELECTEXERCISE, PLACESENSORS, LAUNCHROCKETS, NARRATIVE
+}
