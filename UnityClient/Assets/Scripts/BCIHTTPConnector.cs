370f81381becee9418f11a5903279e7d9d08e9db
Brian Jordan <brian@doborog.com>
2019-01-19 19:44:11 -0500

    Switch to using HTTP requests for BCI data

---
+++ UnityClient/Assets/Scripts/BCIHTTPConnector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BCIHTTPConnector : MonoBehaviour {
+
+	void Start()
+	{
+		StartCoroutine(MakeDataRequest());
+	}
+
+	IEnumerator MakeDataRequest()
+	{
+		UnityWebRequest www = UnityWebRequest.Get("http://93b9d778.ngrok.io/");
+		Debug.Log("Web Request Started");
+		yield return www.SendWebRequest();
+		Debug.Log("Web Request Ended");
+		if (www.isNetworkError || www.isHttpError)
+		{
+			Debug.Log("It errored");
+			Debug.Log(www.error);
+		}
+		else
+		{
+			Debug.Log("It succeeded:");
+			Debug.Log(www.responseCode);
+			Debug.Log("Resp text:");
+			Debug.Log(www.downloadHandler.text);
+			// Or retrieve results as binary data
+//			byte[] results = www.downloadHandler.data;
+		}
+	}
+}
